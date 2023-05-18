b341f7b9754ec1a351918daa05971b448f57eed3
Pawel Wolkow <wolkow937@gmail.com>
2023-05-18 04:01:39 +0200

    lab #7 + CRUD implementation

---
+++ lab6/app/src/Controller/Admin/QuoteCrudController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Quote;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class QuoteCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Quote::class;
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-dashboard');
+        yield MenuItem::linkToCrud('Answers', 'fa-duotone fa-quotes', Quote::class);
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->renderSidebarMinimized()
+            ->setEntityLabelInSingular('Quote')
+            ->setEntityLabelInPlural('Quotes')
+            ->setEntityPermission('ROLE_ADMIN')
+            ->setDefaultSort(['id' => 'ASC', 'historian' => 'ASC', 'year' => 'DESC'])
+            ->setPageTitle('index', '%entity_label_plural% listing')
+            ->setPaginatorPageSize(10)
+            ->setPaginatorRangeSize(2);
+    }
+
+    public function CreateEntity(string $entityFqcn)
+    {
+        return new Quote('New Quote', 'New Historian', '0000');
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('quote'),
+            TextField::new('historian'),
+            IntegerField::new('year'),
+        ];
+    }
+}
--- lab6/app/src/Controller/QuoteController.php
+++ lab6/app/src/Controller/QuoteController.php
@@ -4,18 +4,17 @@ namespace App\Controller;
 
 use App\Repository\QuoteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class QuoteController extends AbstractController
 {
 
-    #[Route('/', name: 'app_quote')]
+    #[Route('/', name: 'homepage')]
     public function index(QuoteRepository $quoteRepository): Response
     {
-        return $this->render('quote/index.html.twig', [
-            'quotes' => $quoteRepository->findAll(),
-        ]);
+        return $this->quotes($quoteRepository);
     }
 
     #[Route('/quotes', name: 'app_quote')]
